b1fe5f7cb3ab58859a7a4c4a17c96830f9d8f65d
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-05 12:31:35 +0530

    05-04-22

--- weatherwebsite/src/app.js
+++ weatherwebsite/src/app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express();
 const port = 5010
+const path = require('path')
 
-app.listen(port, () => {
-    console.log(`server is run on port ${port}`)
-})
+// public static path 
+
+const static_path = (path.join(__dirname, "../public"))
+app.use(express.static(static_path))
+    // Router 
 
 app.get("/", (req, res) => {
     res.send("Welcome to Dhruvik page")
+})
+app.get("/weather", (req, res) => {
+    res.send("Welcome to Dhruvik page")
+})
+app.get("/about", (req, res) => {
+    res.send("Welcome to Dhruvik page")
+})
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is run on port ${port}`)
 })
