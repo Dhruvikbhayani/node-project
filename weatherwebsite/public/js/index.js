4ca15eba38abfcfc2e6dfc204a40256a2ad18e72
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-07 10:48:54 +0530

    07-04-22

---
+++ weatherwebsite/public/js/index.js
@@ -0,0 +1,7 @@
+const submitbutton = document.getElementById('submitbutton')
+
+function reload(event) {
+    event.preventDefault();
+    let url = http: //api.openweathermap.org / data / 2.5 / weather ? q = surat & d = 77662731887ee069bcd6b1bfe3e00608
+}
+submitbutton.addEventListener('click', reload)
--- weatherwebsite/src/app.js
+++ weatherwebsite/src/app.js
@@ -2,21 +2,32 @@ const express = require('express')
 const app = express();
 const port = 5010
 const path = require('path')
-
-// public static path 
+const hbs = require('hbs')
 
 const static_path = (path.join(__dirname, "../public"))
+const templets_path = (path.join(__dirname, "../templets/views"))
+const partials_path = (path.join(__dirname, "../templets/partials"))
+
+
+// public static path 
+app.set('view engine', 'hbs')
+app.set('views', templets_path)
 app.use(express.static(static_path))
-    // Router 
+hbs.registerPartials(partials_path)
+
+// Router 
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Dhruvik page")
+    res.render('index')
 })
 app.get("/weather", (req, res) => {
-    res.send("Welcome to Dhruvik page")
+    res.render('weather')
 })
 app.get("/about", (req, res) => {
-    res.send("Welcome to Dhruvik page")
+    res.render('about')
+})
+app.get("*", (req, res) => {
+    res.render('404err')
 })
 
 
